989a19f588891f53d5ac80b04ded2328e18c2fee
omarahmed98 <keyomar10@gmail.com>
2024-10-08 19:07:01 +0300

    added core, shared, todo modules in app root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { TodoModule } from './todo/todo.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,10 @@ import { EffectsModule } from '@ngrx/effects';
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
     StoreDevtoolsModule.instrument({ maxAge: 10, logOnly: environment.production }),
-    EffectsModule.forRoot([])
+    EffectsModule.forRoot([]),
+    CoreModule,
+    SharedModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
