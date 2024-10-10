32b574a7aa602c35f57049230c3c949956b6d141
omarahmed98 <keyomar10@gmail.com>
2024-10-10 12:52:56 +0300

    implemented the unit testing of service following TDD

--- src/app/todo/services/todo.service.spec.ts
+++ src/app/todo/services/todo.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { TodoService } from './todo.service';
 
+
 describe('TodoService', () => {
   let service: TodoService;
 
@@ -12,5 +13,18 @@ describe('TodoService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return a list of todos', (done) => {
+    const expectedTodos = [
+      { id: 1, title: 'Learn Angular' },
+      { id: 2, title: 'Implement NgRx' }
+    ];
+
+    service.getTodos().subscribe((todos) => {
+      expect(todos.length).toBe(2);
+      expect(todos).toEqual(expectedTodos);
+      done(); // Ensures async observable completes
+    });
   });
 });
