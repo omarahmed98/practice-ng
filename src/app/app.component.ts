8a5167624406b22272701b6618c310351cefd2eb
omarahmed98 <keyomar10@gmail.com>
2024-10-12 00:13:42 +0300

    created using TDD principle app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>Todo App</h1>
+    <app-todo-form></app-todo-form>
+    <app-todo-list></app-todo-list>
+  `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'practice-ng';
-}
+export class AppComponent {}
