350c5ab8290998d730e1c5b926ba2437c0169829
omarahmed98 <keyomar10@gmail.com>
2024-10-12 13:14:56 +0300

    styling body of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Todo App</h1>
-    <app-todo-form></app-todo-form>
-    <app-todo-list></app-todo-list>
+    <div class="todo-app">
+      <h1>Todo App</h1>
+      <app-todo-form></app-todo-form>
+      <app-todo-list></app-todo-list>
+    </div>
   `,
   styleUrls: ['./app.component.css']
 })
